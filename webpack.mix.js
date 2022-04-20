031f1cc3f6608ce7e930598487e29e6a8fca6c88
lolsokje <alex9521@hotmail.com>
2022-04-20 20:02:13 +0200

    Added Mix cache busting

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ const webpackConfig = require('./webpack.config');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/scss/app.scss', 'public/css')
     .webpackConfig(webpackConfig)
-    .vue();
+    .vue()
+    .version();
