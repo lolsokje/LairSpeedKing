1c615b00052c6a796061b2fd2257fb59c970fd74
Shift <shift@laravelshift.com>
2023-02-03 14:05:33 +0000

    Remove `webpack.mix.js`

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-const webpackConfig = require('./webpack.config');
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
-    .webpackConfig(webpackConfig)
-    .vue()
-    .version();
